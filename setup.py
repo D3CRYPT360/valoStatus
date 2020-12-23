d7e57b11541f6b10c524f355b83dc6bc832608b0
D3crypt360 <nahilali3600@gmail.com>
2020-12-23 14:42:37 +0500

    1.0.8

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='valoStatus',
+    version='1.0.8',
+    author='D3CRYPT360',
+    author_email="lordomega360@gmail.com",
+    description='A python library to check for riot games server status for VALORANT without an API key',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/D3CRYPT360/valStatus',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>= 3.6',
+    keywords=['valorant','gaming','gamers'], 
+    include_package_data=True,
+    install_requires=['requests']
+)
